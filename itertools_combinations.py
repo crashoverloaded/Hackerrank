a3f259ce76eb2e3027afb13d0cc2698ff0dac13a
Priyank Agarwal <priyankagarwal00.pa@gmail.com>
2019-10-16 23:13:38 +0530

    Using Itertools

--- itertools_combinations.py
+++ itertools_combinations.py
@@ -1,8 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 from itertools import combinations
 string,n=input().split()
-for i in sorted(string):
-    print(i)
-a=list(combinations(sorted(string),int(n)))
-a=[''.join(i) for i in a]
+a=[]
+for i in range(1,int(n)+1):
+    a.append(list(combinations(sorted(string),i)))
+#a=[''.join(i) for i in a for j in i]
 for i in a:
-    print(i)
+    for j in i:
+        print(''.join(j))
